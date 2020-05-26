6fc8d2b905efb4bfaab3c2e75a5b84675b00aabb
liukai <liukai@yzyw-inc.com>
2020-05-26 17:02:46 +0800

    feat: 回溯算法——0-1背包问题（新增在背包可装载最大重量下求最大价值以及对应的总重量）

--- src/main/java/com/liukai/datastructure/ch_39_backtrack/Backtrack01Pack.java
+++ src/main/java/com/liukai/datastructure/ch_39_backtrack/Backtrack01Pack.java
@@ -1,8 +1,13 @@
 package com.liukai.datastructure.ch_39_backtrack;
 
-import org.apache.commons.lang3.RandomUtils;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.apache.commons.lang3.tuple.Pair;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * 39-2 回溯算法经典应用——0-1 背包问题
@@ -24,7 +29,7 @@ public class Backtrack01Pack {
    *
    * <p>下标表示物品索引，值表示物品的重量
    */
-  private final int[] items;
+  private final ItemInfo[] items;
 
   /**
    * 物品总个数
@@ -41,74 +46,170 @@ public class Backtrack01Pack {
    */
   private int maxW = 0;
 
-  public Backtrack01Pack(int[] itemWeights, int itemNum, int backMaxWeight) {
-    this.items = itemWeights;
+  /**
+   * 存储可以背包中物品总价值的最大值
+   */
+  private int maxV = 0;
+
+  public Backtrack01Pack(ItemInfo[] itemInfos, int itemNum, int backMaxWeight) {
+    this.items = itemInfos;
     this.itemNum = itemNum;
     this.backMaxWeight = backMaxWeight;
   }
 
   public static void main(String[] args) {
-
     // 物品个数
     int itemNum = 10;
-
     // 物品重量
-    int[] itemWeights = new int[itemNum];
+    ItemInfo[] itemInfos = new ItemInfo[itemNum];
     // 初始化物品重量
-    for (int i = 0; i < itemWeights.length; i++) {
-      itemWeights[i] = RandomUtils.nextInt(1, 100);
-    }
+    // for (int i = 0; i < itemInfos.length; i++) {
+    //   // 物品价值
+    //   int value = RandomUtils.nextInt(10, 1000);
+    //   // 物品重量
+    //   int weight = RandomUtils.nextInt(1, 100);
+    //   itemInfos[i] = new ItemInfo(value, weight);
+    // }
+
+    /*
+     {价值=75元, 重量=27kg}
+     {价值=404元, 重量=78kg}
+     {价值=343元, 重量=84kg}
+     {价值=588元, 重量=89kg}
+     {价值=242元, 重量=86kg}
+     {价值=429元, 重量=63kg}
+     {价值=154元, 重量=51kg}
+     {价值=152元, 重量=76kg}
+     {价值=52元, 重量=45kg}
+     {价值=626元, 重量=60kg}
+    */
+    itemInfos[0] = new ItemInfo(75, 27); // 单价：2.7
+    itemInfos[1] = new ItemInfo(404, 78); // 单价：5.1
+    itemInfos[2] = new ItemInfo(343, 84); // 单价：4.8
+    itemInfos[3] = new ItemInfo(588, 89); // 单价：6.6
+    itemInfos[4] = new ItemInfo(242, 86); // 单价：2.8
+    itemInfos[5] = new ItemInfo(429, 63); // 单价：6.8
+    itemInfos[6] = new ItemInfo(154, 51); // 单价：3
+    itemInfos[7] = new ItemInfo(152, 76); // 单价：2
+    itemInfos[8] = new ItemInfo(52, 45); // 单价：1.15
+    itemInfos[9] = new ItemInfo(626, 60); // 单价：10.4
+
+    System.out.println("商品信息：" + Arrays.toString(itemInfos));
+
     // 背包可承受的最大重量
-    int backMaxWeight = 50;
+    int backMaxWeight = 100;
+    Backtrack01Pack pack = new Backtrack01Pack(itemInfos, itemNum, backMaxWeight);
+    Pair<Integer, Integer> weightValuePair = pack.loadPackMaxWeight(true);
 
-    Backtrack01Pack pack = new Backtrack01Pack(itemWeights, itemNum, backMaxWeight);
+    String info = String
+      .format("背包信息，物品个数=%s, 背包容量=%skg, 最可装载的重量=%skg, 最大价值=%s元", itemNum, backMaxWeight,
+              weightValuePair.getLeft(), weightValuePair.getRight());
+    System.out.println(info);
+    /*
+     商品信息：[{价值=75元, 重量=27kg}, {价值=404元, 重量=78kg}, {价值=343元, 重量=84kg}, {价值=588元, 重量=89kg}, {价值=242元, 重量=86kg}, {价值=429元, 重量=63kg}, {价值=154元, 重量=51kg}, {价值=152元, 重量=76kg}, {价值=52元, 重量=45kg}, {价值=626元, 重量=60kg}]
+     当前背包中的物品信息：[]，背包中的总价值：0元，总重量：0kg
+     当前背包中的物品信息：[{价值=626元, 重量=60kg}]，背包中的总价值：626元，总重量：60kg
+     当前背包中的物品信息：[{价值=52元, 重量=45kg}]，背包中的总价值：52元，总重量：45kg
+     当前背包中的物品信息：[{价值=152元, 重量=76kg}]，背包中的总价值：152元，总重量：76kg
+     当前背包中的物品信息：[{价值=154元, 重量=51kg}]，背包中的总价值：154元，总重量：51kg
+     当前背包中的物品信息：[{价值=154元, 重量=51kg}, {价值=52元, 重量=45kg}]，背包中的总价值：206元，总重量：96kg
+     当前背包中的物品信息：[{价值=429元, 重量=63kg}]，背包中的总价值：429元，总重量：63kg
+     当前背包中的物品信息：[{价值=242元, 重量=86kg}]，背包中的总价值：242元，总重量：86kg
+     当前背包中的物品信息：[{价值=588元, 重量=89kg}]，背包中的总价值：588元，总重量：89kg
+     当前背包中的物品信息：[{价值=343元, 重量=84kg}]，背包中的总价值：343元，总重量：84kg
+     当前背包中的物品信息：[{价值=404元, 重量=78kg}]，背包中的总价值：404元，总重量：78kg
+     当前背包中的物品信息：[{价值=75元, 重量=27kg}]，背包中的总价值：75元，总重量：27kg
+     当前背包中的物品信息：[{价值=75元, 重量=27kg}, {价值=626元, 重量=60kg}]，背包中的总价值：701元，总重量：87kg
+     当前背包中的物品信息：[{价值=75元, 重量=27kg}, {价值=52元, 重量=45kg}]，背包中的总价值：127元，总重量：72kg
+     当前背包中的物品信息：[{价值=75元, 重量=27kg}, {价值=154元, 重量=51kg}]，背包中的总价值：229元，总重量：78kg
+     当前背包中的物品信息：[{价值=75元, 重量=27kg}, {价值=429元, 重量=63kg}]，背包中的总价值：504元，总重量：90kg
 
-    pack.loadPackMaxWeight();
-
-    System.out.println(pack);
-    // Backtract01Pack{物品重量信息=[40, 52, 41, 98, 4, 45, 88, 12, 45, 17], 物品个数=10, 背包容量=50kg,
-    // 最大可装载的重量=49kg}
+     isNeedMaxValueForWeight 为 false 时，背包信息，物品个数=10, 背包容量=100kg, 最可装载的重量=96kg, 最大价值=701元
+     isNeedMaxValueForWeight 为 true 时， 背包信息，物品个数=10, 背包容量=100kg, 最可装载的重量=87kg, 最大价值=701元
+    */
   }
 
   /**
-   * 求背包中可以装的最大重量
+   * 求背包中可以装的最大重量和允许的最大价值
+   *
+   * @param isNeedMaxValueForWeight 是否求最大价值下的重量，如果为 true，则返回在背包可承载的重量一定的情况下，最大价值下对应的总重量；如果为
+   *                                false，则返回背包可承载的重量一定的情况下，能装载的最大重量，和能支持的最大价值（最大重量和最大价值没有关联）。
+   * @return 最大重量，最大价值
    */
-  public void loadPackMaxWeight() {
-    packMaxWeight(0, 0);
+  public Pair<Integer, Integer> loadPackMaxWeight(boolean isNeedMaxValueForWeight) {
+    packMaxWeight(0, 0, 0, Collections.emptyList(), isNeedMaxValueForWeight);
+    return Pair.of(maxW, maxV);
   }
 
   /**
    * 求背包中可以装的最大重量
    *
-   * @param i  当前考察到哪个商品
-   * @param cw 当前已经装进背包的物品的重量和
+   * @param i                       当前考察到哪个商品
+   * @param cw                      当前已经装进背包的物品的重量和
+   * @param cv                      当前已经装进背包的物品的价值和
+   * @param cItems                  当前背包的物品信息
+   * @param isNeedMaxValueForWeight
    * @desc 假设背包可承受的重量是 100，物品个数 10，物品重量存储在数组 a 中，可这样调用函数 f(0, 0, a, 10, 100)
    */
-  private void packMaxWeight(int i, int cw) {
+  private void packMaxWeight(int i, int cw, int cv, List<ItemInfo> cItems,
+                             boolean isNeedMaxValueForWeight) {
 
     if (i == itemNum || cw == backMaxWeight) {
       // i == n 表示已经考察完所有的物品，cw == w 表示已经装满
-      if (cw > maxW) {
-        maxW = cw;
+
+      if (isNeedMaxValueForWeight) {
+        // 背包信息，物品个数=10, 背包容量=100kg, 最大可装载的重量=87kg, 最大价值=701元
+        if (cw > maxW && cv > maxV) {
+          maxW = cw;
+          maxV = cv;
+        }
+      } else {
+        // 背包信息，物品个数=10, 背包容量=100kg, 最大可装载的重量=96kg, 最大价值=701元
+        if (cw > maxW) {
+          maxW = cw;
+        }
+        if (cv > maxV) {
+          maxV = cv;
+        }
       }
+
+      System.out.println("当前背包中的物品信息：" + cItems + "，背包中的总价值：" + cv + "元，总重量：" + cw + "kg");
       return;
     }
 
+    // 当前背包中物品信息
+    List<ItemInfo> newCItems = new ArrayList<>(cItems);
+
     // 这里很巧妙！
     // 物品不装进背包
-    packMaxWeight(i + 1, cw);
+    packMaxWeight(i + 1, cw, cv, newCItems, isNeedMaxValueForWeight);
 
     // 已经超过了背包可承受的最大重量时，就不要再装了
-    if (cw + items[i] <= backMaxWeight) {
+    ItemInfo item = items[i];
+    if (cw + item.getWeight() <= backMaxWeight) {
       // 物品装进背包
-      packMaxWeight(i + 1, cw + items[i]);
+      newCItems.add(item);
+      packMaxWeight(i + 1, cw + item.getWeight(), cv + item.getValue(), newCItems,
+                    isNeedMaxValueForWeight);
     }
   }
 
-  @Override
-  public String toString() {
-    return "Backtract01Pack{" + "物品重量信息=" + Arrays.toString(items) + ", 物品个数=" + itemNum + ", 背包容量="
-      + backMaxWeight + "kg, 最大可装载的重量=" + maxW + "kg}";
-  }
+  /**
+   * 物品信息
+   */
+  @Data
+  @AllArgsConstructor
+  static class ItemInfo {
 
+    /** 价值 */
+    int value;
+
+    /** 重量 */
+    private int weight;
+
+    @Override
+    public String toString() {
+      return "{" + "价值=" + value + "元, 重量=" + weight + "kg}";
+    }
+  }
 }
